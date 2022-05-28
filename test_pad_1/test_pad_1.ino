d5bb11a5a89c0abf43401e4c1bd4e15cbbd703ec
Francesco Barraco <fran.barr@hotmail.it>
2022-05-28 15:43:44 +0200

    updated arduino for 5 pins

--- test_pad_1/test_pad_1.ino
+++ test_pad_1/test_pad_1.ino
@@ -1,110 +1,167 @@
-#define KICK_PIN  5 // +0
-#define SNARE_PIN 6 // +1
-#define CLAP_PIN 7 // +2
-#define HIHAT_PIN 8 // +3
-#define HARP_PIN 9 // +4
 
-#define SYNTH_PITCH_PIN A0 //+5
+//ANALOGS
+#define TEMPO_PIN A0 // +1 145 - 133
+#define DRUM_VOLUME_PIN A1// +2 146 - 134
+#define INSTRUMENT_VOLUME_PIN A2// +3 147 - 135
+#define KEY_PIN A3 // +4 148 - 136
+//A4 +5 149 - 137
+//A5 +6 150 - 138
 
-#define MIDI_NOTE_ON  (0x90)
-#define MIDI_NOTE_OFF   (0x80)
+//MIDI
+#define MIDI_NOTE_ON  (0x90) // = 144
+#define MIDI_NOTE_OFF   (0x80) // = 132
 
 //WIP: try to use the midi.h library to handle everything, add a manual serial.print('start_letter') to let Supercollider know the start/end of a message.           
 
-bool oldReadKick = false;
-bool oldReadSnare = false;
-bool oldReadClap = false;
-bool oldReadHiHat = false;
-bool oldReadHarp = false;
-bool synthFlag = false;
+//Old reads flag
+bool oldReadPin[12];
 
-bool newReadKick = false;
-bool newReadSnare = false;
-bool newReadClap = false;
-bool newReadHiHat = false;
-bool newReadHarp = false;
-//bool newReadSynthFlag = false;
+//New reads flag
+bool newReadPin[12];
 
-int newReadSynth = 0;
-int oldReadSynth = 0;
+//Analog readings
+int newReadTempo = 0;
+int newReadInstrumentVolume = 0;
+int newReadDrumVolume = 0;
+int newReadKey = 0;
+
+int oldReadTempo = 0;
+int oldReadInstrumentVolume = 0;
+int oldReadDrumVolume = 0;
+int oldReadKey = 0;
+
+bool tempoOnFlag = false;
+bool instrumentOnFlag = false;
+bool drumOnFlag = false;
+bool keyOnFlag = false;
+
+int stepThreshold = 1;
+int offThreshold = 1;
+
+// -------------------------------------------------------- //
+// --------------------- SETUP ---------------------------- //
 
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(115200);
-  pinMode(KICK_PIN,INPUT);
-  pinMode(SNARE_PIN,INPUT);
-  pinMode(CLAP_PIN,INPUT);
-  pinMode(HIHAT_PIN,INPUT);
-  pinMode(HARP_PIN,INPUT);
-  pinMode(SYNTH_PITCH_PIN,INPUT);
+
+  //INSTRUMENT
+  for (int i = 2; i < 14; i++){
+    pinMode(i,INPUT);
+  }
+
+  //ANALOGS
+  pinMode(TEMPO_PIN,INPUT);
+  pinMode(DRUM_VOLUME_PIN,INPUT);
+  pinMode(INSTRUMENT_VOLUME_PIN,INPUT);
+  pinMode(KEY_PIN,INPUT);
+
+
+//Flag array initialization
+  for(int i=0; i<12; i++){
+    oldReadPin[i] = false;
+    newReadPin[i] = false;
+  }
+  
 }
+
+// ------------------------------------------------ //
+// --------------------- LOOP --------------------- //
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  newReadKick = !digitalRead(KICK_PIN);
-  newReadSnare = !digitalRead(SNARE_PIN);
-  newReadClap = !digitalRead(CLAP_PIN);
-  newReadHiHat = !digitalRead(HIHAT_PIN);
-  newReadHarp = !digitalRead(HARP_PIN);
 
-/*
-  if((newReadKick != oldReadKick) && newReadKick){ //KICK OFFSET : +0 (channel 0)
-    midiMsg(MIDI_NOTE_ON,0x1E,0x7F); //note on, max velocity, 0x1E = 30 (0 midi)
-  }else if((newReadKick != oldReadKick) && !newReadKick){
-    midiMsg(MIDI_NOTE_OFF,0x1E,0x7F); //note off, max velocity, 0x1E = 30 (0 midi)
+  //  DIGITAL READINGS
+  // Pullup reading, here negated to be used as usual
+  // Everything on channel 0: [144 NOTE_ON - 132 NOTE_OFF]
+  // Use the pitch message of the midi protocol to indicate the pin
+  // Max velocity fixed for now but the protocol may be changed
+
+  for(int i = 0; i < 12; i++){
+
+    if(i<5
+    )
+      newReadPin[i] = !digitalRead(i+2);
+    //Serial.println(newReadPin[i]);
+    //delay(500);
+    
+    if((newReadPin[i] != oldReadPin[i]) && newReadPin[i]){
+      midiMsg(MIDI_NOTE_ON, i ,0x7F);
+    }else if((newReadPin[i] != oldReadPin[i]) && !newReadPin[i]){
+      midiMsg(MIDI_NOTE_OFF, i ,0x7F);
+    }
+    
+    oldReadPin[i] = newReadPin[i];
+    
   }
 
-  if((newReadSnare != oldReadSnare) && newReadSnare){ //SNARE OFFSET : +1 (channel 1)
-    midiMsg(MIDI_NOTE_ON + 1,0x1E,0x7F); //note on, max velocity, 0x1E = 30 (0 midi)
-  }else if((newReadSnare != oldReadSnare) && !newReadSnare){
-    midiMsg(MIDI_NOTE_OFF + 1,0x1E,0x7F); //note off, max velocity, 0x1E = 30 (0 midi)
-  }
-
-  if((newReadClap != oldReadClap) && newReadClap){ //CLAP OFFSET : +2 (channel 2)
-    midiMsg(MIDI_NOTE_ON + 2,0x1E,0x7F); //note on, max velocity, 0x1E = 30 (0 midi)
-  }else if((newReadClap != oldReadClap) && !newReadClap){
-    midiMsg(MIDI_NOTE_OFF + 2,0x1E,0x7F); //note off, max velocity, 0x1E = 30 (0 midi)
-  }
-
-  if((newReadHiHat != oldReadHiHat) && newReadHiHat){ //HIHAT OFFSET : +3 (channel 1)
-    midiMsg(MIDI_NOTE_ON + 3,0x1E,0x7F); //note on, max velocity, 0x1E = 30 (0 midi)
-  }else if((newReadHiHat != oldReadHiHat) && !newReadHiHat){
-    midiMsg(MIDI_NOTE_OFF + 3,0x1E,0x7F); //note off, max velocity, 0x1E = 30 (0 midi)
-  }
-  */
-
-  if((newReadHarp != oldReadHarp) && newReadHarp){ //HARP OFFSET : +4 (channel 1)
-    midiMsg(MIDI_NOTE_ON + 4,0x1E,0x7F); //note on, max velocity, 0x1E = 30 (0 midi)
-  }else if((newReadHarp != oldReadHarp) && !newReadHarp){
-    midiMsg(MIDI_NOTE_OFF + 4,0x1E,0x7F); //note off, max velocity, 0x1E = 30 (0 midi)
-  }
-
-  oldReadKick = newReadKick;
-  oldReadSnare = newReadSnare;
-  oldReadClap = newReadClap;
-  oldReadHiHat = newReadHiHat;
-  oldReadHarp = newReadHarp;
+  //ANALOG READINGS
 
   //ANALOG INPUT
-  newReadSynth = analogRead(SYNTH_PITCH_PIN);
-  //Serial.println(newReadSynth);
-  //delay(500);
-  newReadSynth = 255 - ((int)newReadSynth/4); // from 0-1023 to 0-255
-  
-  if(newReadSynth > 1){
-    if(abs(newReadSynth - oldReadSynth) > 1){
-      midiMsg(MIDI_NOTE_ON +5, newReadSynth, 0x7F);
-      synthFlag = false;
-      oldReadSynth = newReadSynth;
+  newReadTempo = analogRead(TEMPO_PIN);
+  newReadDrumVolume = analogRead(DRUM_VOLUME_PIN);
+  newReadInstrumentVolume = analogRead(INSTRUMENT_VOLUME_PIN);
+  newReadKey = analogRead(KEY_PIN);
+
+  //Conversion from 1023 - 0 to 0 - 255 (the reading is done in pull)
+  newReadTempo = 255 - ((int)newReadTempo/4);
+  newReadDrumVolume = 255 - ((int)newReadDrumVolume/4);
+  newReadInstrumentVolume = 255 - ((int)newReadInstrumentVolume/4);
+  newReadKey = 255 - ((int)newReadKey/4);
+
+  //Tempo
+  if(newReadTempo > offThreshold){
+    if(abs(newReadTempo - oldReadTempo) > stepThreshold){
+      midiMsg(MIDI_NOTE_ON +1, newReadTempo, 0x7F);
+      tempoOnFlag = true;
+      oldReadTempo = newReadTempo;
     }
-  } else if (!synthFlag) {
-    midiMsg(MIDI_NOTE_OFF + 5, newReadSynth, 0x7F);
-    synthFlag = true;
+  } else if (tempoOnFlag) {
+    midiMsg(MIDI_NOTE_OFF + 1, newReadTempo, 0x7F);
+    tempoOnFlag = false;
   }
+
+  //Drum volume
+  if(newReadDrumVolume > offThreshold){
+    if(abs(newReadDrumVolume - oldReadDrumVolume) > stepThreshold){
+      midiMsg(MIDI_NOTE_ON +2, newReadDrumVolume, 0x7F);
+      drumOnFlag = true;
+      oldReadDrumVolume = newReadDrumVolume;
+    }
+  } else if (drumOnFlag) {
+    midiMsg(MIDI_NOTE_OFF + 2, newReadDrumVolume, 0x7F);
+    drumOnFlag = false;
+  }
+
+  //Instrument Volume
+  if(newReadInstrumentVolume > offThreshold){
+    if(abs(newReadInstrumentVolume - oldReadInstrumentVolume) > stepThreshold){
+      midiMsg(MIDI_NOTE_ON +3, newReadInstrumentVolume, 0x7F);
+      instrumentOnFlag = true;
+      oldReadInstrumentVolume = newReadInstrumentVolume;
+    }
+  } else if (instrumentOnFlag) {
+    midiMsg(MIDI_NOTE_OFF + 3, newReadInstrumentVolume, 0x7F);
+    instrumentOnFlag = false;
+  }
+
+  //Key
+  if(newReadKey > offThreshold){
+    if(abs(newReadKey - oldReadKey) > stepThreshold){
+      midiMsg(MIDI_NOTE_ON +4, newReadKey, 0x7F);
+      keyOnFlag = true;
+      oldReadKey = newReadKey;
+    }
+  } else if (keyOnFlag) {
+    midiMsg(MIDI_NOTE_OFF + 4, newReadKey, 0x7F);
+    keyOnFlag = false;
+  }   
     
-    
-  delay(1);
+  delay(1); //optional
+  
 }
+
+// --------------------------------------------- //
+// -------- FUNCTIONS -------------------------- //
 
 void midiMsg(int cmd, int pitch, int velocity) {
   //Serial.print('z'); // z = midi message start
